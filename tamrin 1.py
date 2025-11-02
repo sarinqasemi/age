729a5c91eb545c957f2f6fece79d4827d1e832ab
sarinqasemi <saringhasemi88@gmail.com>
2025-11-02 18:33:07 +0330

    add tamrin1.py

---
+++ tamrin 1.py
@@ -0,0 +1,8 @@
+#Sarina Ghasemi code 3
+name = input("Please enter your name:")
+birth = input("Please enter your year of birth:")
+birth = int(birth)
+current_year = 1404
+age = current_year - birth
+print(f"Hello, {name}! You are approximately {age} years old.")
+ 
---
+++ tamrin 2.py
@@ -0,0 +1,17 @@
+def avarage(num):
+    return sum(num) / len(num)
+
+
+numbers = int(input("enter number of lessons:"))
+grades = []
+for i in range(numbers):
+    grade = int(input(f"enter lessons {i+1}:"))
+    grades.append(grade)
+
+avg = avarage(grades)
+if avg >= 17:
+    print("great")
+elif avg >= 12:
+    print("passed")
+else:
+    print("conditional")      
